e82c7b2a3b9d771a90172fd591d2489c9728ca72
seancleve <seanycleve@gmail.com>
2024-09-19 13:37:53 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,44 @@ public class Main
 {
     public static void main(String[] args)
     {
-      
+     int OperandA = 33;
+     int OperandB = 21;
+
+     int Sum = 0;
+     int Product = 0;
+     int Difference = 0;
+     int Quotient = 0;
+     int Modulo = 0;
+
+     double doubleOperandA = 66;
+     double doubleOperandB = 42;
+
+     double doubleSum = 0;
+     double doubleProduct = 0;
+     double doubleDifference = 0;
+     double doubleQuotient = 0;
+     double doubleModulo = 0;
+
+
+
+     Sum = OperandA + OperandB;
+        System.out.println("The sum of " + OperandA + " and " + OperandB + " is " + Sum);
+
+     Product = OperandA * OperandB;
+        System.out.println("The product of " + OperandA + " and " + OperandB + " is " + Product);
+
+     Quotient = OperandA / OperandB;
+        System.out.println("The quotient of " + OperandA + " and " + OperandB + " is " + Quotient);
+
+     Difference = OperandA - OperandB;
+        System.out.println("The difference of " + OperandA + " and " + OperandB + " is " + Difference);
+
+     Modulo = OperandA % OperandB;
+        System.out.println("The modulo of " + OperandA + " and " + OperandB + " is " + Modulo);
+
+
+     double lunchCost = 15.50;
+
 
 
 
